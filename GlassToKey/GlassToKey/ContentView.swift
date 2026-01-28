cb9247b024d42138a71959277e81d8f422231f6e
disarmyouwitha <disarmyouwitha@gmail.com>
2026-01-27 20:01:03 -0600

    white bg

--- GlassToKey/GlassToKey/ContentView.swift
+++ GlassToKey/GlassToKey/ContentView.swift
@@ -1984,7 +1984,7 @@ struct ContentView: View {
                 let backgroundRect = CGRect(origin: .zero, size: size)
                 context.fill(
                     Path(backgroundRect),
-                    with: .color(.clear)
+                    with: .color(.white)
                 )
                 ContentView.drawTrackpadContents(
                     context: &context,
