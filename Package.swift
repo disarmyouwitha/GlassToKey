328e0a699428b2e511631ce8798f6a5ff3273b03
disarmyouwitha <disarmyouwitha@gmail.com>
2026-02-04 23:35:18 -0600

    clean

--- Package.swift
+++ Package.swift
@@ -21,10 +21,10 @@ let package = Package(
         .binaryTarget(
             name: "OpenMultitouchSupportXCF",
             // For development: use local framework
-            // path: "OpenMultitouchSupportXCF.xcframework"
+            path: "OpenMultitouchSupportXCF.xcframework"
             // For release: use GitHub URL (replaced by release script)
-            url: "https://github.com/disarmyouwitha/GlassToKey/releases/download/v2.0.1/OpenMultitouchSupportXCF.xcframework.zip",
-            checksum: "5b69b4e180daffaff85fa76e4a211e0290a7ab8c4b49be31aa962bda1fe40fc0"
+            // url: "https://github.com/disarmyouwitha/GlassToKey/releases/download/v2.0.1/OpenMultitouchSupportXCF.xcframework.zip",
+            // checksum: "5b69b4e180daffaff85fa76e4a211e0290a7ab8c4b49be31aa962bda1fe40fc0"
         ),
         .target(
             name: "OpenMultitouchSupport",
